f41014f1e0e0c032e533d22f898ee191c99a251c
Moose-Tracks <86276057+Moose-Tracks@users.noreply.github.com>
2021-12-04 00:07:50 -0800

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -39,5 +39,62 @@ public class Main {
         //result = result - 2
         result -=2; // 10 - 2 = 8
         System.out.println("10 - 2 = " + result);
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an Alien!");
+            System.out.println("And I am scared of Aliens");
+        }
+
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+
+        int secondTopScore = 81;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score, less than 100 ");
+        }
+
+
+        if ((topScore > 90)||(secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+
+        isCar = true;
+
+        //Ternary operator allows programers to set boolean variables to trigger other values to be set to variables...;
+        //Intuitively, behaves as a trigger using boolean logic.;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
+
+        //Challenge
+        double myDouble = 20.00D;
+        double mySecondDouble = 80.00D;
+        double myCombinedDouble = (myDouble + mySecondDouble) * 100D;
+        double resultSubOne = myCombinedDouble % 40.00D;
+        boolean resultbool = (resultSubOne == 0);
+        System.out.println("My result = " + resultbool);
+        if (!resultbool) {
+            System.out.println("Got some Remainder");
+        }
     }
 }
