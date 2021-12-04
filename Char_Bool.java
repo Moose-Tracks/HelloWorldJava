7e9ee64f53951be54dedd836ef10f9e7c01ca4f3
Moose-Tracks <86276057+Moose-Tracks@users.noreply.github.com>
2021-12-03 18:15:58 -0800

    Create Char_Bool.java

---
+++ Char_Bool.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myCharUnicode = '\u0044';
+        System.out.println("My Char = " + myChar);
+        System.out.println("My Char Unicode 1 = " + myCharUnicode);
+        char myCopyright = '\u00A9';
+        System.out.println("My Char Unicode 2 = " + myCopyright);
+
+
+        //Boolean Section
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        
+        //Common Syntax for boolean variable set
+
+        boolean isCustomerOverTwentyOne = true;
+
+    }
+}
