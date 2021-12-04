6a3f5372a2bc1b422fb644f72eeaa62fd46fa803
Moose-Tracks <86276057+Moose-Tracks@users.noreply.github.com>
2021-12-03 20:43:42 -0800

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,33 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //
+        //Byte
+        //short
+        //int
+        //long
+        //float
+        //double
+        //char
+        //boolean
+        String myString = "This is a string!";
+        System.out.println("My string = " + myString);
+        myString = myString+ ", and this is more.";
+        System.out.println("My string = " + myString);
+
+        myString = myString + " \u00A9 2021";
+        System.out.println("My string = " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString =   lastString + myInt;
+        System.out.println("Last String = " + lastString);
+        double doubleNumber= 120.47D;
+        lastString = lastString + doubleNumber;
+        System.out.println("Last String = " + lastString);
+    }
+}
